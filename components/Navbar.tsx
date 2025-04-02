bd520878f2dd29db32858ce355ba8c392baf6e77
YvetteGahamanyi <ygahamanyi26@gmail.com>
2025-04-02 18:52:33 +0200

    refactor

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <Link href="/" className="text-xl font-semibold text-primary">
-            ThriftHub
+            ThriftStore
           </Link>
 
           {/* Desktop Navigation */}
